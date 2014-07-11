43e11a5763c1b2389d6be55b6d6876cac921882d
赵舟 <hzzhaozhou@corp.netease.com>
2014-07-11 15:48:15 +0800

    add some code

---
+++ src/main/java/zzhao/code/concurrent/ThreadDeathWatcher.java
@@ -0,0 +1,152 @@
+/*
+ * @(#) ThreadWatcher.java 2014-7-11
+ * 
+ * Copyright 2013 NetEase.com, Inc. All rights reserved.
+ */
+package zzhao.code.concurrent;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.log4j.Logger;
+
+import com.google.common.collect.Lists;
+
+/**
+ * 从netty那边看到的，监控线程是不是死了,如果死了就执行一下指定的代码
+ * @author hzzhaozhou
+ * @version 2014-7-11
+ */
+public class ThreadDeathWatcher {
+    private static final Logger logger = Logger.getLogger(ThreadDeathWatcher.class);
+
+    private static final LinkedBlockingDeque<Entry> pendingEntries = new LinkedBlockingDeque<Entry>();
+    private static final Watcher watcher = new Watcher();
+    private static final AtomicBoolean started = new AtomicBoolean();
+    private static volatile Thread watcherThread;
+
+    /**
+     * Schedules the specified {@code task} to run when the specified {@code thread} dies.
+     *
+     * @param thread the {@link Thread} to watch
+     * @param task the {@link Runnable} to run when the {@code thread} dies
+     *
+     * @throws IllegalArgumentException if the specified {@code thread} is not alive
+     */
+    public static void watch(Thread thread, Runnable task) {
+        if (thread == null) {
+            throw new NullPointerException("thread");
+        }
+        if (task == null) {
+            throw new NullPointerException("task");
+        }
+        if (!thread.isAlive()) {
+            throw new IllegalArgumentException("thread must be alive.");
+        }
+
+        pendingEntries.add(new Entry(thread, task));
+
+        if (started.compareAndSet(false, true)) {
+            Thread watcherThread = new Thread(watcher);
+            thread.setDaemon(true);
+            watcherThread.start();
+            ThreadDeathWatcher.watcherThread = watcherThread;
+        }
+    }
+
+    /**
+     * Waits until the thread of this watcher has no threads to watch and terminates itself.
+     * Because a new watcher thread will be started again on {@link #watch(Thread, Runnable)},
+     * this operation is only useful when you want to ensure that the watcher thread is terminated
+     * <strong>after</strong> your application is shut down and there's no chance of calling
+     * {@link #watch(Thread, Runnable)} afterwards.
+     *
+     * @return {@code true} if and only if the watcher thread has been terminated
+     */
+    public boolean awaitInactivity(long timeout, TimeUnit unit) throws InterruptedException {
+        if (unit == null) {
+            throw new NullPointerException("unit");
+        }
+
+        Thread watcherThread = ThreadDeathWatcher.watcherThread;
+        if (watcherThread != null) {
+            watcherThread.join(unit.toMillis(timeout));
+        }
+        return !watcherThread.isAlive();
+    }
+
+    private ThreadDeathWatcher() {
+    }
+
+    private static final class Watcher implements Runnable {
+
+        private final List<Entry> watchees = Lists.newArrayList();
+
+        @Override
+        public void run() {
+            for (;;) {
+                fetchWatchees();
+                notifyWatchees();
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignore) {
+                    // Ignore the interrupt; do not terminate until all tasks are run.
+                }
+
+                if (watchees.isEmpty() && pendingEntries.isEmpty()) {
+
+                    boolean stopped = started.compareAndSet(true, false);
+                    assert stopped;
+
+                    if (pendingEntries.isEmpty()) {
+                        break;
+                    }
+
+                    if (!started.compareAndSet(false, true)) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void fetchWatchees() {
+            for (;;) {
+                Entry e = pendingEntries.poll();
+                if (e == null) {
+                    break;
+                }
+
+                watchees.add(e);
+            }
+        }
+
+        private void notifyWatchees() {
+            Iterator<Entry> itor = this.watchees.iterator();
+            while (itor.hasNext()) {
+                Entry e = itor.next();
+                if (!e.thread.isAlive()) {
+                    itor.remove();
+                    try {
+                        e.task.run();
+                    } catch (Throwable t) {
+                        logger.warn("Thread death watcher task raised an exception:", t);
+                    }
+                }
+            }
+        }
+    }
+
+    private static final class Entry {
+        final Thread thread;
+        final Runnable task;
+
+        Entry(Thread thread, Runnable task) {
+            this.thread = thread;
+            this.task = task;
+        }
+    }
+}
---
+++ src/main/java/zzhao/code/file/FileContentUtils.java
@@ -0,0 +1,77 @@
+/*
+ * @(#) FileContentUtils.java 2014-5-29
+ * 
+ * Copyright 2013 NetEase.com, Inc. All rights reserved.
+ */
+package zzhao.code.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.hssf.record.formula.functions.T;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+/**
+ *
+ * @author hzzhaozhou
+ * @version 2014-5-29
+ */
+public class FileContentUtils {
+
+    public static void writeToFile(Collection<T> contents, String file) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        for (Object obj : contents) {
+            writer.write(obj.toString() + "\n");
+        }
+        writer.close();
+    }
+
+    public static List<String> readAsList(String file) {
+        File in = new File(file);
+        List<String> result = Lists.newLinkedList();
+        try {
+            FileReader fr = new FileReader(in);
+            BufferedReader reader = new BufferedReader(fr);
+            String data = reader.readLine();
+            while (StringUtils.isNotEmpty(data)) {
+                result.add(data);
+                data = reader.readLine();
+            }
+            reader.close();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static Set<String> readAsSet(String file) {
+        File in = new File(file);
+        Set<String> result = Sets.newLinkedHashSet();
+        try {
+            FileReader fr = new FileReader(in);
+            BufferedReader reader = new BufferedReader(fr);
+            String data = reader.readLine();
+            while (StringUtils.isNotEmpty(data)) {
+                result.add(data);
+                data = reader.readLine();
+            }
+            reader.close();
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
